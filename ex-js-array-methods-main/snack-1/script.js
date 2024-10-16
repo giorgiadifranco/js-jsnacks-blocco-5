debece93916ee2d9dd1ebdde8ec11f42cb3676f3
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-16 14:32:15 +0200

    add snack 1 forEach

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,5 +1,26 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+console.log(names);
+
+ names.forEach(( nome ) => {
+    console.log(nome);
+    
+ })
+
+
+ 
+
+ for (let i = 0 ; i <names.length; i++){
+    
+    console.log(names[i]);
+     
+     
+ }
+ 
+
+
+
+
 
 
 // Stampa in console tutti i nomi
