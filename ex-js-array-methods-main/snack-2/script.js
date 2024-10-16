ae1e5f7cdf52fc5aee541cca565785600f83651b
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-16 14:44:20 +0200

    add snack 2 map, forEach e loop For

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -3,6 +3,34 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+//metodo con map per stampare nuovo array di soli nomi
+const newPeople = people.map((person) => {
+  return person.name
+}) 
+console.log(newPeople);
+
+//metodo forEach per stampare il singolo elemento
+
+const new_people = people.forEach((person)=>{
+ console.log(person.name)
+})
+
+
+
+//ciclo for per nuovo array al posto di map
+
+const new_People =[]
+for(let i = 0; i< people.length; i++){
+    let person = people[i];
+
+    new_People.push(person)
+
+}
+console.log(new_People);
+
+
+
+
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
