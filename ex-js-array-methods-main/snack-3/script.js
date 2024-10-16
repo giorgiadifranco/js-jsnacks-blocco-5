24fbbc067d34d7be5019e4965ba92e19ebb8cc5c
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-16 15:11:12 +0200

    add snack3 map method

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const newArrnumbers = numbers.map(number => number+1)
+
+console.log(newArrnumbers);
+
+
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
